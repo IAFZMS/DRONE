984837abb692841993bed12c5d97d0b7b496e23e
IAFZMS <ingaeronauticofzms@gmail.com>
2019-11-21 01:58:49 -0600

    agregue cambios en drone.py solo de pruba

--- drone.py
+++ drone.py
@@ -4,8 +4,7 @@ from django.db import models
 
 class Drone(models.Model):
     nombre = models.CharField(max_lenggth=200, null=False, blank=False)
-    año = models.CharField(max_length=4 null=False, blank=False)
+    año = models.CharField(max_length=200 null=False, blank=False)
     autonomia = models.CharField(max_length=200, null=False, blank=False)
     num_motores = models.CharField(max_length=2, null=False, blank=False)
     costo = models.CharField(max_length=6, null=False, blank=False)
-    
